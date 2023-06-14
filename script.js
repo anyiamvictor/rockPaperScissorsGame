f92d6fd102ae93b071a05b8666d0b7e0025c3782
anyiamvictor <anyaimvictor@gmail.com>
2023-06-14 09:49:42 +0100

    building the user interface part1

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 "use strict"
 const arr = ['ROCK', 'PAPER', 'SCISSORS'];
-const score = [0,0];
+const score = [0, 0];
+const btnPlay = document.querySelectorAll('.btnPlay');
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
 
 const generateRandomNumber =function(){
   // Generate a random decimal between 0 and 2
@@ -19,6 +23,7 @@ function getComputerChoice() {
 }
 
 
+
 const playRound = function (playerSelection, computerSelection) {
     const playerChoice = prompt('Enter Paper, Rock or Scissors');
     playerSelection = playerChoice.toUpperCase();    
@@ -27,47 +32,46 @@ const playRound = function (playerSelection, computerSelection) {
     if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
         console.log('You Win!','Player:', playerSelection, 'Computer:',computerSelection)
         score[0]++;
-        console.log(score)
+        console.log(score);
     }
      if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
         console.log('You Win!','Player:', playerSelection, 'Computer:',computerSelection)
          score[0]++;
-         console.log(score)
+         console.log(score);
          
     } if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
         console.log('You Win!')
         score[0]++;
-         console.log(score)
+        console.log(score);
 
     }
     if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
          console.log('You Lose!','Player:', playerSelection, 'Computer:',computerSelection, score)
         score[1]++;
-         console.log(score)
+        console.log(score);
 
     }
      if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
          console.log('You Lose!','Player:', playerSelection, 'Computer:',computerSelection, score)
          score[1]++;
-         console.log(score)
+         console.log(score);
 
      }
       if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
-         console.log('You Lose!','Player:', playerSelection, 'Computer:',computerSelection, score)
+          console.log('You Lose!','Player:', playerSelection, 'Computer:',computerSelection, score)
           score[1]++;
-         console.log(score)
+          console.log(score);
 
       }
-        
-        
-        
 }
 
 
-function game(){
-    for (let i = 1; i <= 5; i++) {
-         playRound();
-    }    
-}
-game();
-console.log('Your score', score);
+
+
+
+// function game(){
+//    playRound();
+        
+// }
+// game();
+// console.log('Your score', score);
